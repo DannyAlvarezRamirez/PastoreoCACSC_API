dffa4902a652cd05cc7adbf381e906c9b1a3f92d
Danny Alvarez <dany.alvarez@uned.cr>
2024-10-04 16:20:32 -0500

    updates

--- Models/UserUpdateRequest.cs
+++ Models/UserUpdateRequest.cs
@@ -5,6 +5,6 @@
         public string FullName { get; set; }
         public string Email { get; set; }
         public decimal RolId { get; set; }
-        public string ModificadoPor { get; set; }
+        public string ModificadoPor { get; set; } 
     }
 }
